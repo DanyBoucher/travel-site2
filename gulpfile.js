4817d018905127148c0724a1cebab8e22d962eaa
Dany <danyboucher99@gmail.com>
2018-08-12 13:23:52 -0400

    4.12 Gulp and PostCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require("gulp"),
-watch = require("gulp-watch");
+watch = require("gulp-watch"),
+postcss = require("gulp-postcss"),
+autoprefixer = require("autoprefixer"),
+cssvars = require("postcss-simple-vars"),
+nested = require("postcss-nested");
 
 gulp.task("default", function() {
   console.log("Hooray - you created a Gulp task.")
@@ -10,12 +14,14 @@ gulp.task("html", function() {
 });
 
 gulp.task("styles", function() {
-  console.log("Imagine Sass or PostCSS tasks running here.");
+  return gulp.src("./app/assets/styles/styles.css")
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest("./app/temp/styles"));
 });
 
 
 
-
+// _____________________________________________________________
 gulp.task("watch", function() {
   watch("./app/index.html", function() {
     gulp.start("html");
